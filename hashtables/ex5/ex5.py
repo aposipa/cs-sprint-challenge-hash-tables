fab18f9da7fad287c41641ee3f2a9302992c5500
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-06-05 13:56:19 -0400

    ex5 passing some tests

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -2,33 +2,40 @@
 
 import re
 
+# def finder(files, queries):
+#     #files is an array of paths
+#     #queries is an array of filenames
+
+#     file_dict = {}
+
+
+#     for path in files:
+#         for filename in queries:
+#             if filename in path:
+#                 file_dict[path] = filename
+#                 break
+
+#     # result = [val for key, val in test_dict.items() if search_key in key]
+#     result = []
+#     for path in file_dict:
+#         result.append(path)
+#     #return all the filepaths that match a query
+#     return result
+
 def finder(files, queries):
-    #files is an array of paths
-    #queries is an array of filenames
+    q_str = ', '.join(queries)
 
-    file_dict = {}
-
-    # for filename in queries:
-    #     if filename not in file_dict:
-    #         file_dict[filename] = len(filename)
-
-    for path in files:
-        if path not in file_dict:
-            file_dict[path] = None
-
+    path_dict = {}
     result = []
-    for filename in queries:
-        for path in files:
-            if filename in path:
-                result.append(path)
+    for path in files:
+        path_arr = path.split('/')
+        path_dict[path] = path_arr[-1]
 
-    
+    for path in path_dict:
+        if path_dict[path] in q_str:
+            result.append(path)
 
-
-
-    #return all the filepaths that match a query
     return result
-
 
 if __name__ == "__main__":
     files = [
